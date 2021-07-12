3d08d1cb962dd4d057e33d735fd8d0a3f932fec6
wmd4@njit.edu <wmd4@njit.edu>
2021-07-12 19:01:18 -0400

    Task: New add method works

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,6 +1,12 @@
+def addition(a, b):
+    return a + b
 
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -3,11 +3,18 @@ from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)  # tests if there is a calculator
+        self.assertIsInstance(self.calculator, Calculator)  # tests if there is a calculator
 
+    def test_results_property_calculator(self):
+        self.assertEqual(self.calculator.result, 0)
+
+    def test_add_method_calculator(self):  # tests that addition works in calculator
+        self.assertEqual(self.calculator.add(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
 
 if __name__ == '__main__':
     unittest.main()
